d49bfc62108391b3a052c714324e28979355f3a4
Dilovan Celik <dilovan@celikconsulting.com>
2024-12-26 21:48:59 +0100

    first commit

---
+++ lua/vim-options.lua
@@ -0,0 +1,30 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set rnu")
+vim.cmd("set nu")
+vim.cmd("set clipboard=unnamed")
+vim.cmd("set updatetime=1000")
+vim.g.mapleader = " "
+ 
+-- setup diagnostics
+vim.diagnostic.config({ virtual_text = false })
+vim.api.nvim_create_autocmd({ "CursorHold" }, {
+	callback = function()
+		if vim.lsp.buf.server_ready() then
+			vim.diagnostic.open_float()
+		end
+	end,
+})
+
+-- set up LSP signs
+for type, icon in pairs({
+	Error = "",
+	Warn = "",
+	Hint = "",
+	Info = "",
+}) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
